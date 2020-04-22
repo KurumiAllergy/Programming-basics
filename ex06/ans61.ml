9b1a70f013cb0bcd1e87e6f6a599278d35db246d
KurumiAllergy <kurumiallergy@gmail.com>
2020-04-22 18:53:01 +0900

    Add ans61.ml

---
+++ ex06/ans61.ml
@@ -0,0 +1,15 @@
+(*let square x = x * x ;;
+*square 3. ;;
+*This expression has type float but an expression was expected of type
+*         int
+*)
+
+(*let circle = 2. *. pi *. r ;;
+*circle 2. ;;
+*Error: Unbound value pi
+*)
+
+(*let TV bangumi youbi = bangumi ^ "は" ^ youbi ^ "に放映です。" ;;
+*TV "ポケモン" "木曜日" ;;
+*Error: Syntax error
+*)
