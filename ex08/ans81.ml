f854af547a97d470dc892e44b0cce4dc15f7db3e
KurumiAllergy <kurumiallergy@gmail.com>
2020-05-05 18:55:42 +0900

    Add ans81.ml

---
+++ ex08/ans81.ml
@@ -0,0 +1,31 @@
+type book_t =  {
+    title : string;
+    tyosya : string;
+    syuppan : string;
+    nedan : int;
+    isbn : string;
+} 
+
+let book1 = { 
+  title = "プログラミングの基礎"; 
+  tyosya = "浅井健一"; 
+  syuppan = "サイエンス社"; 
+  nedan = 2300; 
+  isbn = "978-4-7819-1160-1"; 
+} 
+ 
+let book2 = { 
+  title = "アルゴリズムとデータ構造"; 
+  tyosya = "石畑清"; 
+  syuppan = "岩波書店"; 
+  nedan = 3900; 
+  isbn = "4-00-010343-1"; 
+} 
+ 
+let book3 = { 
+  title = "プログラミング言語 Standard ML 入門"; 
+  tyosya = "大堀淳"; 
+  syuppan = "共立出版"; 
+  nedan = 3800; 
+  isbn = "4-320-12024-8"; 
+} 
