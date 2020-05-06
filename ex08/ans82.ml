c9a70485ed177540fcd82e87cbd1231b66bee6e8
KurumiAllergy <kurumiallergy@gmail.com>
2020-05-06 16:11:52 +0900

    Add ans82.ml and ans83.ml

---
+++ ex08/ans82.ml
@@ -0,0 +1,27 @@
+type okozukai_t = {
+    name : string;
+    nedan : int;
+    basyo : string;
+    hiduke : string;
+}
+
+let item1 = { 
+  name = "プログラミングの基礎"; 
+  nedan = 2415; 
+  basyo = "大学生協書籍部"; 
+  hiduke = "2007/3/20"; 
+} 
+ 
+let item2 = { 
+  name = "午後の紅茶"; 
+  nedan = 150; 
+  basyo = "ローソン"; 
+  hiduke = "2007/3/22"; 
+} 
+ 
+let item3 = { 
+  name = "置き時計"; 
+  nedan = 1200; 
+  basyo = "イトーヨーカドー"; 
+  hiduke = "2007/3/28"; 
+} 
