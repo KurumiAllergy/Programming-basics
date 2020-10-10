d436bcdf9687a548cafdefd7764408578ab531dd
KurumiAllergy <kurumiallergy@gmail.com>
2020-10-10 18:11:56 +0900

    fix

--- src/ans145.ml
+++ src/ans145.ml
@@ -14,6 +14,45 @@ let lst4 = [{namae = "yoshida"; tensuu = 80; seiseki = "A"};
             {namae = "asai"; tensuu = 70; seiseki = "B"}; 
             {namae = "kaneko"; tensuu = 85; seiseki = "A"}]
 
+(* リストの数字が偶数の物だけ抽出 *)
+(* even : int list -> int list *)
+let even lst = 
+  (* 偶数かどうか判定 *)
+  (* mod2_0 : int -> bool *)
+  let mod2_0 x = x mod 2 = 0 in 
+    List.filter mod2_0 lst
+
+(*test*) 
+let test1 = even [] = []
+let test2 = even [1; 2; 3] = [2]
+let test3 = even [2; 4; 6; 8] = [2; 4; 6; 8]
+
+(* A型の人数を返す *)
+(* count_A : gakusei_t list -> int *)
+let count_A lst = 
+  (* 血液型がA型か調べる *)
+  (* is_A : gakusei_t -> bool *)
+  let is_A gakusei = match gakusei with
+  {namae = na; tensuu = te; seiseki = se} -> se = "A" in
+    List.length (List.filter is_A lst)
+
+(* test *)
+let test4 = count_A lst1 = 0
+let test5 = count_A lst2 = 0
+let test6 = count_A lst3 = 1
+let test7 = count_A lst4 = 2
+
+(* 文字列のリストから、その中の要素を前から順に接合した文字列を返す *)
+(* concat : String list -> String *)
+let concat lst = 
+  let ketsugou first rest_result = first ^ rest_result in
+    List.fold_right ketsugou lst ""
+
+(* test *)
+let test8 = concat [] = ""
+let test9 = concat ["一"; "二"; "三"; "四"; "五"] = "一二三四五"
+let test10 = concat ["本日"; "天気"; "晴朗"; "なれども"; "波高し"] = "本日天気晴朗なれども波高し"
+
 (* gakusei_t型のリストを受け取り全員の得点合計を返す *)
 (* gakusei_sum : gakusei_t list -> int *)
 let gakusei_sum lst = 
@@ -24,7 +63,7 @@ let gakusei_sum lst =
     List.fold_right add_tokuten lst 0
 
 (* test *)
-let test1 = gakusei_sum lst1 = 0
-let test2 = gakusei_sum lst2 = 70
-let test3 = gakusei_sum lst3 = 155
-let test4 = gakusei_sum lst4 = 235
+let test11 = gakusei_sum lst1 = 0
+let test12 = gakusei_sum lst2 = 70
+let test13 = gakusei_sum lst3 = 155
+let test14 = gakusei_sum lst4 = 235
